a243d42c9120a5379ec6fa515796a1f907786ce5
Your Name <mari@Maribels-MBP.hsd1.tx.comcast.net>
2021-12-15 17:53:47 -0600

    solved mvp

--- user-onboarding/src/components/NewUser.js
+++ user-onboarding/src/components/NewUser.js
@@ -6,7 +6,7 @@ function NewUser({details}){
       }
       return(
         <div>
-        <h2>{details.username}</h2>
+        <h2>{details.name}</h2>
         <p>Email: {details.email}</p>
         </div>
     )
---
+++ user-onboarding/src/components/form.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+export default function NewUserForm(props){
+const {
+    values,
+    submit,
+    change,
+    disabled,
+    errors,
+    } = props
+const onSubmit = evt => {
+    evt.preventDefault()
+    submit()
+    }
+const onChange = evt => {
+    const {name, value, checked, type} = evt.target
+    const valueToUse = type === 'checkbox' ? checked : value;
+    change (name, valueToUse)
+}
+
+return(
+    <form className = 'form-container' onSubmit = {onSubmit}>
+        <div className = 'form-group submit'>
+         <h2>Add a User</h2>
+         <button disabled={disabled}>submit</button>
+
+         <div className = 'errors'>
+          <div>{errors.first_name}</div>
+          <div>{errors.email}</div>
+          <div>{errors.terms}</div>
+          <div>{errors.password}</div>
+         </div>
+        </div>
+
+        <div className='form-group inputs'>
+        <h4>General information</h4>
+        <label>First Name
+            <input
+            value = {values.name}
+            onChange = {onChange}
+            name = 'name'
+            type = 'text'
+            />
+        </label>
+
+        <label>Password
+            <input
+            value = {values.password}
+            onChange = {onChange}
+            name = 'password'
+            type = 'password'
+            />
+        </label>
+
+        <label>Email
+            <input 
+            value = {values.email}
+            onChange = {onChange}
+            name = 'email'
+            type = 'text'
+            />
+        </label>
+        </div>
+
+        <div className = 'form-group checkboxes'>
+            <label>Terms of Service
+                <input
+                 type = 'checkbox'
+                 name = 'terms'
+                 checked = {values.terms}
+                 onChange = {onChange}
+                />
+            </label>
+        </div>
+
+    </form>
+)
+
+}
