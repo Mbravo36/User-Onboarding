d5340a6f5974d4f13ef0727283c5219d51dfb2c0
Your Name <mari@Maribels-MBP.hsd1.tx.comcast.net>
2021-12-15 15:02:07 -0600

    newuser is set up, form.js created, formschema created, app.js bein worked on

---
+++ user-onboarding/src/components/NewUser.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function NewUser({details}){
+    if (!details) {
+        return <h3>Working fetching your new user&apos;s details...</h3>
+      }
+      return(
+        <div>
+        <h2>{details.username}</h2>
+        <p>Email: {details.email}</p>
+        </div>
+    )
+}
+export default NewUser;
